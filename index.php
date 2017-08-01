b83ec43c31b40584c1be363bebba75f9e36ed263
Harrison Gordon <harrison@Harrisons-MacBook-Pro.local>
2017-08-01 08:18:15 -0700

    removing index.php -> index.html shift

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
- <?php header( 'Location: /index.html' ) ;  ?>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, max-scale=1, min-scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400" rel="stylesheet">	<link type="text/css" rel="stylesheet" href="css/index.css">
+</head>
+<body>
+<div class="image-container">
+	<img src="images/logo-white.svg" />
+</div>
+<h1>SparkMD</h1>
+</body>
